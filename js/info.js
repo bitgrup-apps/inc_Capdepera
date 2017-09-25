bc058326e0ff1b2da4580670bf4c063dbfaac398
TomeuMarkenyo <tmarqueno@bitgrup.com>
2017-09-25 11:50:06 +0200

    2.0.6

--- js/info.js
+++ js/info.js
@@ -456,8 +456,16 @@ var init = {
         var ref = window.open(url, '_system', 'location=yes');
     },
     openRadio: function () {
-        //init.carregaPagExt('http://capdeperaradio.com/radioendirecte/');
-        window.open('http://91.121.156.27:8010/stream', '_self', 'location=yes');
+        var audioElement = document.getElementById('successSound');
+        var url = audioElement.getAttribute('src');
+        var my_media = new Media(url,
+                // success callback
+                 function () { console.log("playAudio():Audio Success"); },
+                // error callback
+                 function (err) { console.log("playAudio():Audio Error: " + err); }
+        );
+               // Play audio
+        my_media.play();
     },
     areYouSure: function (text2, button, callback, back_) {
         $("#sure .sure-2").text(text2);
