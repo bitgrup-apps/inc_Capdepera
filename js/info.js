45dfad50ffb0ff5a16f1155850a69d712b2fb6e4
Aleix <aalcover@bitgrup.com>
2019-11-25 08:10:45 +0100

    idiomes

--- js/info.js
+++ js/info.js
@@ -318,7 +318,7 @@ var init = {
             }
             lang = lang.substr(0, 2);
         }
-        if (lang != 'es' && lang != 'ca') {
+        if (lang != 'es' && lang != 'ca' && lang != 'en' && lang != 'de') {
             lang = 'es';
         }
         init.lang = lang;
