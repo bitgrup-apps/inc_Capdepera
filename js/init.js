f997a9938dceafee8edb4ad03c2f245d2fd6f537
Aleix <aalcover@bitgrup.com>
2019-12-03 12:55:09 +0100

    test

--- js/init.js
+++ js/init.js
@@ -240,20 +240,20 @@ function onMapInit() {
     };
 //
 //    //AGAFAM LA LOCALITZACIÓ
-//    window.mapa.LocationService.getMyLocation(onSuccess, onError);
+    window.mapa.LocationService.getMyLocation(onSuccess, onError);
 //    // SI CLICK GUARDAM NOVA LOCALITZACIÓ
-//    var evtName = plugin.google.maps.event.MAP_CLICK;
-//    window.mapa.on(evtName, function (latLng) {
-//        if (comprovaPosicio(latLng.lat, latLng.lng)) {
-//            window.mapa.trigger("MARKER_REMOVE");
-//            $('#latitutIncidencia').val(latLng.lat);
-//            $('#longitutIncidencia').val(latLng.lng);
-//            const NOVAPOSICIO = new plugin.google.maps.LatLng(latLng.lat, latLng.lng);
-//            novaLocalitzacio(NOVAPOSICIO);
-//        } else {
-//            alert("La nova posició no es troba a una àrea correcte");
-//        }
-//    });
+    var evtName = plugin.google.maps.event.MAP_CLICK;
+    window.mapa.one(evtName, function (latLng) {
+        if (comprovaPosicio(latLng.lat, latLng.lng)) {
+            window.mapa.trigger("MARKER_REMOVE");
+            $('#latitutIncidencia').val(latLng.lat);
+            $('#longitutIncidencia').val(latLng.lng);
+            const NOVAPOSICIO = new plugin.google.maps.LatLng(latLng.lat, latLng.lng);
+            novaLocalitzacio(NOVAPOSICIO);
+        } else {
+            alert("La nova posició no es troba a una àrea correcte");
+        }
+    });
     //ACTUALITZAM LLISTA DE INCIDENCIES
     //iniciaLlistatIncidencies();
 
