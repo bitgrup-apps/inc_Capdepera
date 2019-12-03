1e1c602c9b7292eb39081a945d1a1b6f2fb463b7
Aleix <aalcover@bitgrup.com>
2019-12-03 13:54:21 +0100

    test

--- js/init.js
+++ js/init.js
@@ -200,7 +200,7 @@ function onMapInit() {
                     result.thoroughfare || "",
                     result.locality || "",
                     result.postalCode || ""].join(", ");
-                window.mapa.trigger("MARKER_REMOVE");
+               // window.mapa.trigger("MARKER_REMOVE");
                 window.mapa.addMarker({
                     'position': position,
                     'title': address
