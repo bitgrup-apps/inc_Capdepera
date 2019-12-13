0519338e7506c1a16c8e793fe25d351c8d03d1f6
Aleix <aalcover@bitgrup.com>
2019-12-13 13:09:12 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -233,6 +233,7 @@ getLocation: function() {
 clickEvent: function () {
             var evtName = plugin.google.maps.event.MAP_CLICK;
             mapaInc.mapInc.on(evtName, function (latLng) {
+            console.log('click');
             mapaInc.mapInc.trigger("MARKER_REMOVE");
             $('#latitutIncidencia').val(latLng.lat);
             $('#longitutIncidencia').val(latLng.lng);
