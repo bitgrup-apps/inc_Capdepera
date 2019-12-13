5fe989a5f08061978b112d523fc2f5c931de3d92
Aleix <aalcover@bitgrup.com>
2019-12-13 11:12:03 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -158,8 +158,8 @@ var mapaInc = {
                         'zoom': 18
                     }
                 });},300);               
-                mapaInc.mapInc.setClickable(true);
-                mapaInc.mapInc.getVisibleRegion();
+//                mapaInc.mapInc.setClickable(true);
+//                mapaInc.mapInc.getVisibleRegion();
                 console.log('pre-one');
                 mapaInc.mapInc.one(plugin.google.maps.event.MAP_READY, mapaInc.onMapInit);               
                 console.log('post-one');
