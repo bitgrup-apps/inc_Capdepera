91b0be912a8fa968f514ffbe06aeab727ca085c3
Aleix <aalcover@bitgrup.com>
2019-12-16 10:12:25 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -165,7 +165,7 @@ var mapaInc = {
                
                 //setTimeout(function() { mapaInc.mapInc.one(plugin.google.maps.event.MAP_READY, mapaInc.onMapInit); },350);
                //mapaInc.mapInc.one(plugin.google.maps.event.MAP_READY, mapaInc.onMapInit);
-               mapaInc.mapInc.addEventListenerOnce(plugin.google.maps.event.MAP_READY, mapaInc.onMapInit());
+               mapaInc.mapInc.addEventListener(plugin.google.maps.event.MAP_READY, mapaInc.onMapInit);
                console.log('after one');
               //mapaInc.onMapInit();
                
