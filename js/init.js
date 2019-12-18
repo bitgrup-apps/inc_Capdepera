bdc4090f45371b3d83ac0d153667c8a15e78df6b
Aleix <aalcover@bitgrup.com>
2019-12-18 11:44:55 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -176,7 +176,7 @@ var mapaInc = {
       map: map
     });
     google.maps.event.addListener(map, 'click', function(event){
-       marker.setMap(null);
+       
        var marker = new google.maps.Marker({
        position: event.latLng,
        map: map
