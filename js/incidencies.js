13dd006d104cf84f928fe35f773d1678991da3ef
Aleix <aalcover@bitgrup.com>
2020-01-03 09:21:08 +0100

    canvi BD tokens push

--- js/incidencies.js
+++ js/incidencies.js
@@ -307,7 +307,7 @@ function removeImg(i) {
 
 function guardaTokenPush(token) {
     if (window.okPush) {
-        $.post('http://gestcap.com/gestio/App/incidencies.php',
+        $.post('http://oncapdepera.com/App/tokenPush.php',
          {funcio: 'guardaToken', userToken: token, TOKEN: 'LAIDSD88347ERJKADKFGKAHPF8YA9DF8Y', os: window.platform},
          function (data) {
              try {
@@ -364,7 +364,7 @@ function actualitzaServidorPush(guardaToken) {
     else
         var funcio = 'eliminaToken';
     var ok = true;
-    $.post('http://gestcap.com/gestio/App/incidencies.php',
+    $.post('http://oncapdepera.com/App/tokenPush.php',
      {funcio: funcio, userToken: window.tokenPush, TOKEN: 'LAIDSD88347ERJKADKFGKAHPF8YA9DF8Y', os: window.platform},
      function (data) {
          try {
