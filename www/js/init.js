8f3710b392f4f1e9dc91d1cc98c9ebb3bc0642cc
Aleix <aalcover@bitgrup.com>
2021-06-16 08:44:45 +0200

    Update init.js

--- www/js/init.js
+++ www/js/init.js
@@ -311,7 +311,7 @@ var mapaInc = {
         console.log('getLocation');
         var option = {enableHighAccuracy: true};
         console.log('after option');
-        plugin.google.maps.LocationService.getMyLocation(option, function (location) {
+        plugin.google.maps.LocationService.getMyLocation(function (location) {
             console.log('After getMyLocation');
             //CAMERA POSITION
             var lat = location.latLng.lat;
